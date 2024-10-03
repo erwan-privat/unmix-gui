94764482054c9299eb6cee79ea3e9d0d556a6b7d
erwan-privat <archaoss@live.com>
2024-10-03 03:18:52 +0200

    Programmatic UI

--- src/menu_bar.gd
+++ src/menu_bar.gd
@@ -1,28 +1,66 @@
 extends MenuBar
 
+const SEP := "-"
+const SK := "shortcut"
+const CB := "callback"
+
 var menu = {
 	"Files": {
-		"Open...": _on_files_open,
-		"-": null,
-		"Quit": _on_files_quit,
+		"Open...": {
+			SK: "open",
+			CB: _on_files_open,
 		},
+		SEP: null,
+		"Quit": {
+			SK: "quit",
+			CB: _on_files_quit,
+		},	
+	},
 	"Help": {
-		"GitHub page (web)": _on_help_github,
-		"About": _on_help_about,
+		"GitHub page (web)": {
+			SK: "github",
+			CB: _on_help_github,
+		},
+		"About": {
+			SK: "help",
+			CB: _on_help_about,
+		},
 	},
 }
 
 
 func _ready() -> void:
-	pass
+	for m in menu:
+		var pop := PopupMenu.new()
+		pop.name = m
+
+		var ix:= 0
+		for key in menu[m]:
+			if key == SEP:
+				pop.add_separator()
+			else:
+				var sc = Shortcut.new()
+				pop.add_item(key)
+				# FIXME setup shortcuts
+				#pop.set_item_shortcut(ix, Shortcut.new())
+			++ix
+
+		pop.index_pressed.connect(func (i: int):
+			var sm = menu[m].keys()[i]
+			menu[m][sm].callback.call()
+		)
+		%MenuBar.add_child(pop)
 
 
 func _on_index_pressed(cat, index) -> void:
-	
+	print(cat, index)
+	pass
 
 
 func _on_files_open() -> void:
-	$Files/Open.popup()
+	var fd := FileDialog.new()
+	fd.file_mode = FileDialog.FILE_MODE_OPEN_FILES
+	fd.popup()
 
 
 func _on_files_quit() -> void:
